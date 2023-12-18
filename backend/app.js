f3f40b283cbd640cf28f0a553ab6b349a9965fb6
Muhammad Zaryab Khan <88337527+zaryabkhan864@users.noreply.github.com>
2023-12-18 09:55:42 +0300

    uploading code on live server

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,9 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './config/dbConnect.js';
 import errorMiddleware from './middlewares/errors.js';
+
+
+const path = require('path')
 //Handle Uncaught exceptions
 process.on('uncaughtException', (err) => {
     console.log(`Error: ${err}`);
@@ -25,6 +28,12 @@ app.use('/api/v1', auth);
 
 // Middleware to handle errors
 app.use(errorMiddleware);
+
+//static files 
+app.use(express.static(path.join(__dirname,"./frontend/build")));
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,"./frontend/build/index.html"))
+})
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server on port  ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 })
